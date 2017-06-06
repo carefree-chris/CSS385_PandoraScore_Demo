e9d9b6690b48d2b3f6a69842a78cdf857b6fd37e
carefree-chris <christopherlynn11@gmail.com>
2017-06-05 21:41:18 -0700

    Initial
    Attempting to use yarnspinner for the narrative idea Trayce had. Won't
    be able to finish it before final product.

---
+++ Assets/SceneTransitions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+
+
+public class SceneTransitions : MonoBehaviour {
+
+    /*
+    [YarnCommand("print")]
+    public void Print(string words)
+    {
+        Debug.Log("WORDS: " + words);
+    }
+
+    [YarnCommand("SceneChange")]
+    public void TransitionToScene(string sceneName)
+    {
+        Debug.Log("Transitioning to Next Scene: " + sceneName);
+
+        SceneManager.LoadScene(sceneName);
+    }*/
+
+    #region Yarn Commands
+    [YarnCommand("SceneChange")]
+    public void TransitionToScene(string sceneName)
+    {
+        Debug.Log("Transitioning to Next Scene: " + sceneName);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    #endregion
+}
