613aa5b8ded93a0c0b35cfc5f9371f118c96c03d
Trayce <TrayceLuxtrum@gmail.com>
2017-06-06 12:26:23 -0700

    Added Credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    public GameObject Cred;
+    public GameObject Logo;
+
+	// Use this for initialization
+	void Start () {
+        Cred.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Open()
+    {
+        Cred.gameObject.SetActive(true);
+        Logo.gameObject.SetActive(false);
+    }
+
+    public void Close()
+    {
+        Cred.gameObject.SetActive(false);
+        Logo.gameObject.SetActive(true);
+    }
+}
